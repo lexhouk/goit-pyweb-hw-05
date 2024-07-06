0ea47d74582c120542ab40618c386c9bf1f86703
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-07-06 11:42:58 +0300

    Issue #2: Define some message from the chat as a command to show exchanging rates

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import asyncio
 import logging
+
 import websockets
 import names
 from websockets import WebSocketServerProtocol
 from websockets.exceptions import ConnectionClosedOK
+
+from main import main as exchange
 
 logging.basicConfig(level=logging.INFO)
 
@@ -35,6 +38,22 @@ class Server:
 
     async def distrubute(self, ws: WebSocketServerProtocol):
         async for message in ws:
+            if message == 'exchange':
+                raw: dict = (await exchange(1, []))[0].popitem()[1]
+                items = []
+
+                for currency, rates in raw.items():
+                    records = []
+
+                    for key, value in rates.items():
+                        if value is not None:
+                            records.append(f'{key}: {value} UAH')
+
+                    if records:
+                        items.append(f'{currency} ({", ".join(records)})')
+
+                message = ', '.join(items)
+
             await self.send_to_clients(f"{ws.name}: {message}")
 
 
